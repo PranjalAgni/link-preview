e0907cf255f28ce956faaf83f5ddbd714359cb09
PranjalAgni <pranjal.agnihotri6@gmail.com>
2022-08-06 19:51:01 +0530

    :sparkles: initial commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["v1.tailwindcss.com"]
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
